ef0391368c2206addb499c8a5da763bfe3e8bead
dk <hi@mrkha.ng>
2025-06-01 16:41:05 +0000

    typo fix

--- src/scssTransformer.ts
+++ src/scssTransformer.ts
@@ -7,7 +7,9 @@ const getScssTransformer = async () => {
 };
 
 export const transformScss = async (path: string): Promise<string> => {
-  if (IS_DENO_DEPLOY) return "/* Deno Deploy may not support SCSS in SSR yet";
+  if (IS_DENO_DEPLOY) {
+    return "/* Deno Deploy may not support SCSS in SSR yet */";
+  }
   try {
     const res = await (await getScssTransformer()).compileAsync(path);
     // @NOTE there are other fields under `res`
